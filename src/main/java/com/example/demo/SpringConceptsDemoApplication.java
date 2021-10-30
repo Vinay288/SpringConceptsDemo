87aa57eaa0a890b935993886ddadca3613cdf442
Vinay288 <vinayhiremath288@gmail.com>
2021-10-30 07:54:26 +0530

    ADDED: class to run spring application

--- src/main/java/com/example/demo/SpringConceptsDemoApplication.java
+++ src/main/java/com/example/demo/SpringConceptsDemoApplication.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.example.demo.Component.DemoBean;
 
 @SpringBootApplication
 public class SpringConceptsDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringConceptsDemoApplication.class, args);
+		System.out.print("Welcome to spring concept demo");
+		ApplicationContext context = SpringApplication.run(SpringConceptsDemoApplication.class, args);
+		DemoBean demoBean = context.getBean(DemoBean.class);
+		System.out.println("Demo Bean ="+demoBean.toString());
 	}
 
 }
